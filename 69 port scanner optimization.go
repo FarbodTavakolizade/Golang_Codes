55441bb5472a839219969ba91620fae4724e1959
FarbodTavakolizade <farbodtavakolizade@gmail.com>
2025-03-09 07:16:49 +0330

    Update 69 port scanner optimization.go

--- 69 port scanner optimization.go
+++ 69 port scanner optimization.go
@@ -26,12 +26,12 @@ func main() {
 		go worker(ports, results)
 	}
 	go func() {
-		for i := 1; i <= 2000; i++ {
+		for i := 1; i <= 1024; i++ {
 			ports <- i
 		}
 	}()
 
-	for i := 0; i <= 2000; i++ {
+	for i := 0; i <= 1024; i++ {
 		port := <-results
 		if port != 0 {
 			openports = append(openports, port)
